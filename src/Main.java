a92ea72e22162a12190bb6974399b0781a410335
Alex <89831127161@mail.ru>
2022-10-29 15:53:22 +0300

    Сделано ДЗ

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,14 @@
 public class Main {
-    public static void main(String[] args) {
+    public static void main (String[] args) {
+        Author ivanov = new Author("Ivanov", "Ivan");
+        Book nature = new Book("Nature", ivanov,  2020);
+        Author petrov = new Author("Petrov", "Petr");
+        Book world = new Book("World", petrov, 2018);
+        nature.setYear(2015);
 
-        System.out.println("Hello world!");
+
+
+
+
     }
 }
